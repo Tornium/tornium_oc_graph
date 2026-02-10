fe605f08d95c23659739394fc4857c594424e724
tiksan <webmaster@deek.sh>
2026-02-10 17:15:23 -0600

    Fixed NIF and added basic tests
    Signed-off-by: tiksan <webmaster@deek.sh>

--- lib/tornium_oc_graph/nif.ex
+++ lib/tornium_oc_graph/nif.ex
@@ -4,11 +4,20 @@ defmodule Tornium.OC.Graph.NIF do
 
   @doc false
   def __on_load__ do
-    path = :filename.join(:code.priv_dir(Tornium.OC.Graph), ~c"libtornium_oc_graph_ex")
+    path =
+      :tornium_oc_graph
+      |> :code.priv_dir()
+      |> :filename.join(~c"libtornium_oc_graph")
 
     case :erlang.load_nif(path, 0) do
       :ok -> :ok
       {:error, reason} -> raise "failed to load NIF library, reason: #{inspect(reason)}"
     end
+  end
+
+  @spec calculate_ev(oc_name :: String.t(), success_map :: %{String.t() => float()}) ::
+          {:ok, float()} | {:error, term()}
+  def calculate_ev(oc_name, success_map) when is_binary(oc_name) and is_map(success_map) do
+    :erlang.nif_error("nif not loaded")
   end
 end
--- test/tornium_oc_graph_test.exs
+++ test/tornium_oc_graph_test.exs
@@ -1,8 +1,16 @@
-defmodule TorniumOcGraphTest do
+defmodule Tornium.Test.OC.Graph do
   use ExUnit.Case
-  doctest TorniumOcGraph
 
-  test "greets the world" do
-    assert TorniumOcGraph.hello() == :world
+  test "validate NIF" do
+    assert {:error, nil} = Tornium.OC.Graph.NIF.calculate_ev("foo", %{})
+
+    assert {:ok, ev} =
+             Tornium.OC.Graph.NIF.calculate_ev("Honey Trap", %{
+               "enforcer_1" => 0.0,
+               "muscle_1" => 0.0,
+               "muscle_2" => 0.0
+             })
+
+    assert Float.floor(ev) == 0
   end
 end

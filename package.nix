f9f7a8713ebd167bfe257e527a47b1e16ec29b3c
tiksan <webmaster@deek.sh>
2026-03-11 12:10:38 -0500

    [indev] Added base package.nix file
    Signed-off-by: tiksan <webmaster@deek.sh>

---
+++ package.nix
@@ -0,0 +1,27 @@
+{ lib, python3Packages, pkgs, src, ...}:
+
+python3Packages.buildPythonPackage {
+  pname = "tornium_oc_graph";
+  version = "0.1.2-dev";
+  pyproject = true;
+
+  src = builtins.path{
+    path = "${src}";
+    name = "tornium_oc_graph";
+  };
+
+  nativeBuildInputs = [
+    python3Packages.scikit-build-core
+    pkgs.cmake
+    pkgs.ninja
+  ];
+
+  build-system = with python3Packages; [ scikit-build-core ];
+  propagatedBuildInputs = [];
+  pythonImportCheck = [ "tornium_oc_graph" ];
+
+  meta = with lib; {
+    description = "Tornium OC scenario graphs";
+    license = licenses.asl20;
+  };
+}

9cae17768bf2bfe8339f84aa3a694259364e0018
tiksan <webmaster@deek.sh>
2026-03-11 13:53:05 -0500

    test
    Signed-off-by: tiksan <webmaster@deek.sh>

--- package.nix
+++ package.nix
@@ -17,9 +17,10 @@ python3Packages.buildPythonPackage {
   build-system = with python3Packages; [ scikit-build-core ];
   preConfigure = ''
     mkdir c_src/build/
+    echo $(pwd)
     ls -R .
   '';
-  cmakeDir = "c_src";
+  cmakeDir = "../c_src/build";
 
   propagatedBuildInputs = [];
   pythonImportCheck = [ "tornium_oc_graph" ];
